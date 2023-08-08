8c312e9e252d2d04403f1cc494c8382271ffddbc
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-08 09:05:12 -0500

    feat: add form group to basic-page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class BasicPageComponent {
 
+  //* Declaracion de formulario reactivo en base a FormControls
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(''),
+  //   inStoragr: new FormControl(''),
+  // });
+
+  public myForm: FormGroup = this.formBuilder.group({
+    name: [''],
+    price: [0],
+    inStoragre: [0],
+  })
+
+
+  constructor (private formBuilder: FormBuilder){}
+
+  onSave(): void{
+    console.log(this.myForm.value);
+
+  }
 }
