298d39f033887b39617a3c4b81f33e0c50a997af
JuanPaAvalos <juanpablordz0524@gmail.com>
2023-08-12 04:28:53 -0500

    refactor: change validators from file to service

---
+++ src/app/shared/services/validators.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({ providedIn: 'root' })
+export class ValidatorsService {
+  constructor() {}
+
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+
+
+//* los validadores reciben todo el control que estamos comprobando
+  public cantBeStrider = (control: FormControl): ValidationErrors | null => {
+    const value = control.value.trim().toLowerCase();
+
+    if (value == 'strider') {
+      //* deben retornar un objeto o un null
+      return {
+        noStrider: true,
+      };
+    }
+
+    return null;
+  };
+
+  public isValidField(form: FormGroup, field: string){
+    return form.controls[field].errors && form.controls[field].touched
+  }
+
+
+}
